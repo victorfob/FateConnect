0ab34b5fd9f619bc99c5c6ced02e050a649cb0f3
Victor Brayner <victor.brayner@hotmail.com>
2026-08-22 11:04:01 -0300

    refactor: move the api date helper up to the rides page

--- FateConnect/Web/src/pages/Rides/components/RideFilter/index.tsx
+++ FateConnect/Web/src/pages/Rides/components/RideFilter/index.tsx
@@ -6,7 +6,7 @@ import { Button, Input, Typography } from '@design-system';
 import { FilterAltIcon, SearchIcon } from '@design-system/icons';
 
 import * as C from './constants';
-import { toApiDate } from './helpers/toApiDate';
+import { toApiDate } from '../../helpers/apiDate';
 import * as S from './styles';
 
 type RideFilterProps = Readonly<{ onApply: (filters: RideFilterValues) => void }>;
