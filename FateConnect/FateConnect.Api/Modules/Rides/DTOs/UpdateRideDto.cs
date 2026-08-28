a2787f434ff764282545fdc19c03aa6c9d3275a4
Matheus Lauton <mlautonsantana@gmail.com>
2026-08-28 16:11:43 -0300

    fix(rides): fixing update ride dto

--- FateConnect/FateConnect.Api/Modules/Rides/DTOs/UpdateRideDto.cs
+++ FateConnect/FateConnect.Api/Modules/Rides/DTOs/UpdateRideDto.cs
@@ -5,19 +5,17 @@ using FateConnect.Api.Modules.Rides.Enums;
 
 public record UpdateRideDto
 {
-    [Range(1, 7, ErrorMessage = "The ride must have between 1 and 7 seats.")]
     public int? AvailableSeats { get; init; }
 
-    [StringLength(100, MinimumLength = 3, ErrorMessage = "The destination must be between 3 and 100 characters.")]
+    [StringLength(100)]
     public string? Destination { get; init; }
 
     public DateOnly? DepartureDate { get; init; }
 
     public TimeOnly? DepartureTime { get; init; }
 
-    [EnumDataType(typeof(EnumRideType), ErrorMessage = "Invalid ride type.")]
     public EnumRideType? RideType { get; init; }
 
-    [StringLength(300, ErrorMessage = "The description cannot exceed 300 characters.")]
+    [StringLength(300)]
     public string? Description { get; init; }
 }
