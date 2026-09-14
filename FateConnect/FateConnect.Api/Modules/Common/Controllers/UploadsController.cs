9bceb2062be908854cdaa09314019f4ec908fba7
Matheus Lauton <mlautonsantana@gmail.com>
2026-09-13 21:21:40 -0300

    fix(uploads): enforce strict access control for denunciation images via route separation

--- FateConnect/FateConnect.Api/Modules/Common/Controllers/UploadsController.cs
+++ FateConnect/FateConnect.Api/Modules/Common/Controllers/UploadsController.cs
@@ -1,24 +1,44 @@
 namespace FateConnect.Api.Modules.Common.Controllers;
 
+using FateConnect.Api.Modules.Auth.Attributes;
 using FateConnect.Api.Modules.Common.Enums;
 using FateConnect.Api.Modules.Common.Utils;
+using FateConnect.Api.Modules.Users.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 [ApiController]
 [Route(UploadsLocation.FolderName)]
 [Authorize]
 public class UploadsController(IWebHostEnvironment environment) : ControllerBase
 {
+    [HttpGet("denunciation/{fileName}")]
+    [AuthorizeProfile(EnumProfileType.Administrator)]
+    public ActionResult GetDenunciationImage(string fileName)
+    {
+        return ServeFile(EnumStorageContainer.Denunciation, fileName);
+    }
+
     [HttpGet("{container}/{fileName}")]
-    public ActionResult GetStoredImage(string container, string fileName)
+    [AuthorizeProfile(EnumProfileType.Operator)]
+    public ActionResult GetGenericImage(string container, string fileName)
     {
         bool isValidContainer = Enum.TryParse(container, ignoreCase: true, out EnumStorageContainer storageContainer) && Enum.IsDefined(storageContainer);
 
-        if (!isValidContainer)
+        if (isValidContainer)
             return NotFound();
 
+        if (storageContainer == EnumStorageContainer.Denunciation)
+            return Forbid();
+
+        return ServeFile(storageContainer, fileName);
+    }
+
+    private ActionResult ServeFile(EnumStorageContainer storageContainer, string fileName)
+    {
         bool isKnownImageFormat = ImageContentTypes.TryDescribeStoredFile(fileName, out string? storedFileName, out string? contentType);
 
         if (!isKnownImageFormat)
